633ba71259497e437838e8e485284a9833187070
leoCaversan <leocaversan7@gmail.com>
2023-09-29 22:26:24 -0300

    apps react to view load balancer run

---
+++ react-app-2/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        App-2
+      </h1>
+    </div>
+  );
+}
+
+export default App;
